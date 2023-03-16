3b444d4097743da8c25a232ded66fd69ec726581
windows <windows@microsoft.com>
2023-03-16 13:11:06 +0530

    add paragraph

--- src/Home.js
+++ src/Home.js
@@ -20,7 +20,10 @@ function Home() {
           <h1 className='first'>this structure for multi element</h1>
           <h2 className='second'>jdfhsjkdfhjskdhfj</h2>
           <p style={{color:'red',backgroundColor:'blue'}}>inline css</p>
-          <p style={{'color':'red','background-color':'blue'}}>inline css</p>
+          <p style={{'color':'red','background-color':'blue'}}>inline cssInstead, it will 
+          copy all the configuration files and the transitive dependencies
+           (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except eject will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except eject will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except eject will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except eject will still work, but they will point to the copied scripts so you can tweak them. 
+          At this point you're on your own. </p>
     </Fragment>
   )
   
